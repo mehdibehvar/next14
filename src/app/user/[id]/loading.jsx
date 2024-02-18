4d24f23d72f4856d965e2848c8fc2aa6fe21ee68
mehdibehvar <mehdibehvar00@gmail.com>
2024-02-19 01:16:48 +0330

    del-prev

--- src/app/user/[id]/loading.jsx
+++
@@ -1,8 +0,0 @@
-
-const loading = () => {
-  return (
-    <div><h1>loading............</h1></div>
-  )
-}
-
-export default loading
--- src/app/users/new/page.jsx
+++
@@ -1,8 +0,0 @@
-
-const newUserPage = () => {
-  return (
-    <div>this is new user page</div>
-  )
-}
-
-export default newUserPage
